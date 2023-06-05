0eabf6e3694a28f0a5a47b7faf0632cd01ad353b
Ivan Tung <ivan.tungtatsing@gmail.com>
2023-06-05 14:18:06 +0800

    optimized fetch data

--- Screener/rs_data/data_persist/test.py
+++ Screener/rs_data/data_persist/test.py
@@ -1,70 +1,48 @@
-# import library
 import requests
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
 from yahooquery import Ticker
 import json
-import hashlib
-import logging
-from multiprocessing import Pool
-from functools import partial
+
+def get_latest_data():
+    url = "https://dumbstockapi.com/stock?format=tickers-only&countries=US"
+    with requests.Session() as session:
+        response = session.get(url)
+        data_json = response.json()
+    return data_json
 
 def fetch_data(ticker, symbol):
+    profile = ticker.get_modules('assetProfile')[symbol]
     try:
-        industry = ticker.summary_profile.get(symbol,{}).get("industry")
-        sector = ticker.summary_profile.get(symbol,{}).get("sector")
-    except Exception as e:
-        print(f"{symbol}: data fetch failed: {e}")
+        industry = profile.get("industry")
+        sector = profile.get("sector")
+    except:
+        print(f"{symbol}: data fetch failed")
         return None
     return {"info": {"industry":industry, "sector":sector}}
 
-def update_data_for_symbol(symbol, fetched_data, exist_data):
-    if fetched_data is None:
-        return
-    if symbol in exist_data and exist_data[symbol] == fetched_data:
-        logging.info(f"{symbol}: data was up to date")
-    else:
-        exist_data[symbol] = fetched_data
-        logging.info(f"{symbol}: data updated successfully")
+def update_data(data_json):
+    with open('test_tickers_info.json', 'r') as exist_json:
+        exist_data = json.load(exist_json)
 
-def update_data(latest_data):
-    try:
-      with open('/test_tickers_info.json', 'r') as exist_json:
-          exist_data = json.load(exist_json)
-    except (FileNotFoundError, json.JSONDecodeError):
-        exist_data = {}
-    
-    if not isinstance(exist_data, dict):
-        exist_data = {}
+    symbols = {key.strip(): None for key in data_json}
+    for symbol in symbols:
+        ticker = Ticker(symbol)
+        fetched_data = fetch_data(ticker, symbol)
 
-    symbols = set(key.strip() for key in latest_data)
+        if fetched_data is None:
+            continue
 
-    retries = Retry(total=5, backoff_factor=0.1, status_forcelist = [ 500, 502, 503, 504 ])
-    session = requests.Session()
-    adapter = HTTPAdapter(max_retries= retries)
-    adapter.init_poolmanager(connections = 10, maxsize = 10, max_retries = retries)
-    session.mount('https://', adapter)
-    session.mount('http://', adapter)
-    session.timeout = (30, 30)
+        if symbol in exist_data:
+            if exist_data[symbol] != fetched_data:
+                exist_data[symbol] = fetched_data
+                print(f"{symbol}: data updated successfully")
+            else:
+                print(f"{symbol}: data was up to date")
+        else:
+            exist_data[symbol] = fetched_data
+            print(f"{symbol}: data add successfully")
 
-    pool = Pool()
-    fetch_data_func = partial(fetch_data, Ticker(symbols=symbols, session = session))
-    fetch_data_list = pool.map(fetch_data_func, symbols)
-    pool.close()
-    pool.join()
-
-    fetch_data_dict = {}
-
-    for symbol, fetched_data in zip(symbols, fetch_data_list):
-        fetch_data_dict[symbol] = fetched_data
-    
-    for symbol, fetched_data in fetch_data_dict.items():
-        update_data_for_symbol(symbol, fetched_data, exist_data)
-
-    with open("/test_tickers_info.json", "w") as outfile:
+    with open('test_tickers_info.json", "w") as outfile:
         json.dump(exist_data, outfile, indent=2)
 
-if __name__ == '__main__':
-    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
-    latest_data = requests.get("https://dumbstockapi.com/stock?format=tickers-only&countries=US").json()
-    update_data(latest_data)
+latest_data = get_latest_data()
+update_data(latest_data)
