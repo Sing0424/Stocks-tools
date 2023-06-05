7c4e1a4b55ce32f29c054d17b74ab2564d686fd9
Ivan Tung <ivan.tungtatsing@gmail.com>
2023-06-05 18:05:20 +0800

    update

--- Screener/rs_data/data_persist/test.py
+++ Screener/rs_data/data_persist/test.py
@@ -2,6 +2,7 @@ import requests
 from yahooquery import Ticker
 import json
 
+
 def get_latest_data():
     url = "https://dumbstockapi.com/stock?format=tickers-only&countries=US"
     with requests.Session() as session:
@@ -9,24 +10,29 @@ def get_latest_data():
         data_json = response.json()
     return data_json
 
-def fetch_data(ticker, symbol):
-    profile = ticker.get_modules('assetProfile')[symbol]
+
+def fetch_data(symbol):
+    ticker = Ticker(symbol)
     try:
+        profile = ticker.get_modules("assetProfile")[symbol]
         industry = profile.get("industry")
         sector = profile.get("sector")
-    except:
-        print(f"{symbol}: data fetch failed")
+    except Exception as e:
+        print(f"{symbol}: data fetch failed, {e}")
         return None
-    return {"info": {"industry":industry, "sector":sector}}
+    return {"info": {"industry": industry, "sector": sector}}
+
 
 def update_data(data_json):
-    with open(r'C:\Ivan\Repo\Stocks-tools\Screener\rs_data\data_persist\test_tickers_info.json', 'r') as exist_json:
+    with open(
+        r"C:\Ivan\Repo\Stocks-tools\Screener\rs_data\data_persist\test_tickers_info.json",
+        "r",
+    ) as exist_json:
         exist_data = json.load(exist_json)
 
     symbols = {key.strip() for key in data_json}
     for symbol in symbols:
-        ticker = Ticker(symbol)
-        fetched_data = fetch_data(ticker, symbol)
+        fetched_data = fetch_data(symbol)
 
         if fetched_data is None:
             continue
@@ -41,8 +47,12 @@ def update_data(data_json):
             exist_data[symbol] = fetched_data
             print(f"{symbol}: data add successfully")
 
-    with open(r"C:\Ivan\Repo\Stocks-tools\Screener\rs_data\data_persist\test_tickers_info.json", "w") as outfile:
+    with open(
+        r"C:\Ivan\Repo\Stocks-tools\Screener\rs_data\data_persist\test_tickers_info.json",
+        "w",
+    ) as outfile:
         json.dump(exist_data, outfile, indent=2)
+
 
 latest_data = get_latest_data()
 update_data(latest_data)
